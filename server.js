bf196982a0e11f468fcf6fd2cadd5fcd5eb45973
houari006 <102093582+houari006@users.noreply.github.com>
2025-11-08 14:51:59 +0100

    prepare for vercel deploy

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ dotenv.config();
 const app = express();
 app.use(cors());
 app.use(bodyParser.json());
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 const SECRET_KEY = process.env.SECRET_KEY || "mysecretkey";
 
 // ===================================================
